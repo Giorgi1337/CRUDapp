be3e5ec5e962c4b37e094f883885ac80ea03f419
Giorgi1337 <kvacabaya02@gmail.com>
2024-02-01 20:22:43 +0400

    connect postgresql

--- src/main/java/spring/mvc/dao/PersonDAO.java
+++ src/main/java/spring/mvc/dao/PersonDAO.java
@@ -1,45 +1,43 @@
 package spring.mvc.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import spring.mvc.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private final JdbcTemplate jdbcTemplate;
 
-    {
-        people = new ArrayList<>();
-
-        people.add(new Person(++PEOPLE_COUNT,"Giorgi",21,"kvacabaya02@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Anna",25,"Anna25@gmai.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike",30, "supermike@yeahoo.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy", 29,"Katkat29@gmail.com"));
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
-        return people;
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
+
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id)
-                .findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
+
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail());
     }
 
-    public void update(int id, Person updatedperson) {
-        Person personToBeUpdated = show(id);
-
-        personToBeUpdated.setName(updatedperson.getName());
-        personToBeUpdated.setAge(updatedperson.getAge());
-        personToBeUpdated.setName(updatedperson.getName());
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
--- src/main/java/spring/mvc/models/Person.java
+++ src/main/java/spring/mvc/models/Person.java
@@ -3,16 +3,22 @@ package spring.mvc.models;
 import jakarta.validation.constraints.*;
 
 public class Person {
-    public Person() {}
     private int id;
+
     @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2,max = 30, message = "Name should be between 2 and 30 characters")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
-    @Min(value = 0, message = "Can be only positive numbers")
+
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
     private String email;
+
+    public Person() {
+
+    }
 
     public Person(int id, String name, int age, String email) {
         this.id = id;
@@ -36,6 +42,7 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
     public int getAge() {
         return age;
     }
